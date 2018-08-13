b728436996bd17d87002c213ea92230b042dc34c
NaPrikole <p.v.v1313@gmail.com>
2018-08-13 23:39:03 +0300

    first commit

---
+++ web.entry.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import './style.scss';
+import store from './store';
+import moment from 'moment-timezone';
+import App from './components/App.vue';
+
+moment.tz.setDefault('UTC');
+Object.defineProperty(Vue.prototype, '$moment', { get() {return this.$root.moment}});
+
+new Vue({
+  el: '#app',
+  data: {
+    moment
+  },
+  components: {
+      App
+  },
+  store
+});
